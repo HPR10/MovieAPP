cfbc54de36f3ace6423b60454895f8869917efb2
Hugo Pinheiro <hugo_hp10@hotmail.com>
2023-04-27 23:40:14 -0300

    Hello World

---
+++ MovieAPP/MovieAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAPPApp.swift
+//  MovieAPP
+//
+//  Created by HP on 27/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
